390516fac034731f476e7b58a0805675f61ee2a4
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-01-19 00:01:38 +0100

    fix(lint) fix error for linter config

--- src/presentation/shared/LayoutDashboard.tsx
+++ src/presentation/shared/LayoutDashboard.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/presentation/components/dashboard/shared/appSidebar';
-import BreadcrumbDashboard from '@/presentation/components/dashboard/breadcrumb';
+/* import BreadcrumbDashboard from '@/presentation/components/dashboard/breadcrumb'; */
 import SearchBar from '../components/dashboard/shared/SearchBar';
 
 export default function LayoutDashboard({ children }: { children: React.ReactNode }) {
@@ -9,8 +9,6 @@ export default function LayoutDashboard({ children }: { children: React.ReactNod
       <AppSidebar />
       <SidebarTrigger className='md:hidden pl-2' />
       <main className="w-full bg-white bg-gradient-to-b">
-        <div className="flex w-full items-center gap-2 px-4 py-2">
-        </div>
         <div className="mx-6 pt-4"> 
           <SearchBar />    
         {children}
