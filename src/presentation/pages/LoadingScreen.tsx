c993a37c34a64eca5a028c41baf36859784b962b
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-01-21 23:59:43 +0100

    style(loading) add styled components Loading screen

--- src/presentation/pages/LoadingScreen.tsx
+++ src/presentation/pages/LoadingScreen.tsx
@@ -1,7 +1,36 @@
+import styled, { keyframes } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  height: 8rem;
+  width: 8rem;
+  border-radius: 50%;
+  border: 2px solid transparent;
+  border-top-color: #0077ff;
+  border-bottom-color: #0077ff;
+  animation: ${spin} 1s linear infinite;
+`;
+
 const LoadingScreen = () => (
-  <div className="flex h-screen w-full items-center justify-center">
-    <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-blue-500"></div>
-  </div>
+  <Container>
+    <Spinner />
+  </Container>
 );
 
 export default LoadingScreen;
