99016ffd47447e5272684b341e9da46c95a22451
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-01-22 00:20:42 +0100

    style(home) add styled components home

--- src/presentation/pages/home/Hero.tsx
+++ src/presentation/pages/home/Hero.tsx
@@ -1,28 +1,95 @@
+import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { Button } from '../../../components/ui/button';
+import Button from '@/presentation/components/ui/button/Button';
 import learn from '@/assets/learn-min.jpg';
+
+const Container = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+`;
+
+const ImageWrapper = styled.div`
+  position: absolute;
+  inset: 0;
+  opacity: 1;
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(to right, rgba(255, 255, 255, 1) 40%, rgba(255, 255, 255, 0) 65%);
+    pointer-events: none;
+  }
+`;
+
+const BackgroundImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  object-position: right top;
+`;
+
+const ContentContainer = styled.div`
+  position: absolute;
+  inset: 2.25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+const TextContainer = styled.div`
+  margin-bottom: 1rem;
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+    width: 50%;
+  }
+`;
+
+const Title = styled.h1`
+  color: #333;
+  margin-bottom: 1.5rem;
+  font-size: 2.25rem;
+  font-weight: 800;
+  line-height: 1.2;
+
+  @media (min-width: 768px) {
+    font-size: 3.75rem;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 1.25rem;
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+  color: #8a8a8a;
+`;
 
 const Hero = () => {
   const { t } = useTranslation();
+
   return (
-    <div className="relative h-screen w-full" id="home">
-      <div className="absolute inset-0 opacity-60">
-        <img
-          src={learn}
-          alt="Background Image"
-          className="h-full w-full object-cover object-right-top"
-        />
-      </div>
-      <div className="absolute inset-9 flex flex-col items-center justify-center md:flex-row md:justify-between">
-        <div className="mb-4 md:mb-0 md:w-1/2">
-          <h1 className="text-grey-700 mb-6 text-4xl font-extrabold leading-tight md:text-6xl">
-            {t('home.title')}
-          </h1>
-          <p className="font-regular mb-8 mt-2 text-balance text-xl">{t('home.description')}</p>
-          <Button>{t('home.cta')}</Button>
-        </div>
-      </div>
-    </div>
+    <Container id="home">
+      <ImageWrapper>
+        <BackgroundImage src={learn} alt="Background Image" />
+      </ImageWrapper>
+      <ContentContainer>
+        <TextContainer>
+          <Title>{t('home.title')}</Title>
+          <Description>{t('home.description')}</Description>
+          <Button variant="primary">{t('home.cta')}</Button>
+        </TextContainer>
+      </ContentContainer>
+    </Container>
   );
 };
 
