ebe4aa0fddce356b05cea69f9299e2e3a36b0094
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-02-11 16:12:21 +0100

    feat(dashboard) refactor CardFolder component for improved layout and styling

--- src/presentation/components/dashboard/folders/CardFolder.tsx
+++ src/presentation/components/dashboard/folders/CardFolder.tsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import { useProfileUserById } from '@/presentation/hooks/useProfileUserById';
-import { styled } from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { US } from 'country-flag-icons/react/3x2';
-import { FR } from 'country-flag-icons/react/3x2';
-
 import { Folder } from '@/domain/entities/Folder';
-
 import { formatDate } from '@/lib/FormatDate';
-
 import Card from '@mui/joy/Card';
-import { Typography } from '@mui/joy';
+import CardContent from '@mui/joy/CardContent';
+import CardActions from '@mui/joy/CardActions';
+import Typography from '@mui/joy/Typography';
+import Avatar from '@mui/joy/Avatar';
+import Box from '@mui/joy/Box';
 
 const CardFolder = ({ ...props }: Folder) => {
   const { id, name, thema, lang, user_id, created_at } = props;
@@ -19,88 +17,45 @@ const CardFolder = ({ ...props }: Folder) => {
 
   return (
     <Card
-      id={id}
+      variant="outlined"
       sx={{
-        gap: 3,
+        maxWidth: '100%',
+        boxShadow: 'sm',
       }}
     >
       <CardContent>
-        <Typography level="h2" sx={{ fontSize: '1rem', fontWeight: 500 }}>
+        <Typography level="h2" fontSize="lg" fontWeight="lg">
           <Link to={`/dashboard/folders/${id}`}>{name}</Link>
         </Typography>
-        <Typography level="h3" sx={{ fontSize: '0.75rem', fontWeight: 400 }}>
+        <Typography level="h3" color="secondary" fontSize="sm">
           {thema}
         </Typography>
       </CardContent>
-
       {!isLoading && profile && (
-        <ContainerFooter>
-          <Avatar src={`/src/assets/${profile.avatar}.webp`} alt="avatar user" />
-          <CardFooter>
-            <Typography level="h4" sx={{ fontSize: '0.75rem', fontWeight: 500 }}>
-              {t('dashboard.folder.by')} {profile?.firstname}
+        <CardActions
+          sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+        >
+          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+            <Avatar src={profile.avatar} alt="avatar user" />
+            <Box>
+              <Typography level="h4" fontSize="sm" color="secondary">
+                {t('dashboard.folder.by')} {profile?.firstname}
+              </Typography>
+              <Typography fontSize={12}> {created_at ? formatDate(created_at) : ''}</Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+            <Typography fontSize={14} fontWeight={600}>
+              {t('language')}:
             </Typography>
-            <Typography level="h4" sx={{ fontSize: '0.75rem', fontWeight: 400 }}>
-              {created_at ? formatDate(created_at) : ''}
+            <Typography fontSize={14} fontWeight="bold">
+              {lang.toUpperCase()}
             </Typography>
-            <ConatainerLang>
-              <Typography sx={{ fontSize: '0.75rem', fontWeight: 500 }}>{t('language')}</Typography>
-              {lang === 'fr' ? (
-                <FlagIcon>
-                  <FR />
-                </FlagIcon>
-              ) : (
-                <FlagIcon>
-                  <US />
-                </FlagIcon>
-              )}
-            </ConatainerLang>
-          </CardFooter>
-        </ContainerFooter>
+          </Box>
+        </CardActions>
       )}
     </Card>
   );
 };
-
-const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-  gap: 0.2rem;
-`;
-
-const CardFooter = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const ContainerFooter = styled.div`
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-`;
-
-const ConatainerLang = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-`;
-
-const FlagIcon = styled.div`
-  width: 20px;
-  height: 20px;
-
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const Avatar = styled.img`
-  height: 3.5rem;
-  width: 3.5rem;
-  border-radius: 8px;
-`;
 
 export default CardFolder;
