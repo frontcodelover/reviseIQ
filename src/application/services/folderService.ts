0ead8a7d38efefc8f625ba523f1dcbb73d24b3bc
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-02-15 03:15:19 +0100

    feat(services) refactor folderService to use appContainer for fetching folder names

--- src/application/services/folderService.ts
+++ src/application/services/folderService.ts
@@ -1,12 +1,8 @@
-import { GetFolderById } from '@/application/useCases/folder/GetFolderById.usecase';
-import { SupabaseFolderRepository } from '@/infrastructure/backend/SupabaseFolderRespository';
-
-const folderRepository = new SupabaseFolderRepository();
-const getFolderById = new GetFolderById(folderRepository);
+import { appContainer } from '@/infrastructure/config/AppContainer';
 
 const fetchFolderName = async (id: string): Promise<string> => {
   try {
-    const folder: { name: string } = await getFolderById.execute(id);
+    const folder: { name: string } = await appContainer.getFolderService().getFolderById(id);
     return folder.name;
   } catch (error) {
     console.error('Erreur lors de la récupération du nom du dossier :', error);
