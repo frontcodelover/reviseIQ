5c920c67220562a82601749cca546884da5757e1
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-02-21 15:14:51 +0100

    refactor(signup) extract some logic

---
+++ src/lib/ValidatePassword.ts
@@ -0,0 +1,9 @@
+export function validatePassword(password: string) {
+  return {
+    hasUpperCase: /[A-Z]/.test(password),
+    hasLowerCase: /[a-z]/.test(password),
+    hasNumber: /[0-9]/.test(password),
+    hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+    minLength: password.length >= 6,
+  };
+}
