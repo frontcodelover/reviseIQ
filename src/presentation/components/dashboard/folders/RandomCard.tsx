e9e320f7c48d5763c596936121830693d92cfd57
frontcodelover <98742791+frontcodelover@users.noreply.github.com>
2025-02-22 15:18:23 +0100

    feat(RandomCard) refactor component to use new UI elements and improve layout

--- src/presentation/components/dashboard/folders/RandomCard.tsx
+++ src/presentation/components/dashboard/folders/RandomCard.tsx
@@ -1,95 +1,31 @@
 import { Folder } from '@/domain/entities/Folder';
+import { Card, CardContent, CardFooter } from '@/presentation/components/ui/card';
 import { useProfileUserById } from '@/presentation/hooks/useProfileUserById';
-import { Typography } from '@mui/joy';
-import Card from '@mui/joy/Card';
-import { US } from 'country-flag-icons/react/3x2';
-import { FR } from 'country-flag-icons/react/3x2';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { styled } from 'styled-components';
 
-const RandomCard = ({ ...props }: Folder) => {
-  const { id, name, lang, user_id } = props;
+export function RandomCard({ id, name, lang, user_id }: Folder) {
   const { profile, isLoading } = useProfileUserById(user_id || '');
   const { t } = useTranslation();
 
   return (
-    <Card
-      id={id}
-      sx={{
-        border: 'none',
-        boxShadow: '0 0 2px 0 rgba(0 0 0 / 0.2),0 5px 17px -4px rgba(0 0 0 / 0.12)',
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '0.5rem',
-        padding: '1rem',
-        backgroundColor: 'var(--joy-palette-primary-plainHoverBg)',
-        color: 'white',
-      }}
-    >
-      <CardContent>
-        <Typography level="h2" sx={{ fontSize: '1rem', fontWeight: 500 }}>
-          <Link to={`/dashboard/folders/${id}`}>{name}</Link>
-        </Typography>
+    <Card className="flex h-full w-full min-w-96 flex-col gap-2 p-4 hover:bg-primary/90">
+      <CardContent className="flex h-full flex-col justify-between gap-1 p-0">
+        <Link to={`/dashboard/folders/${id}`} className="text-base font-medium hover:underline">
+          {name}
+        </Link>
       </CardContent>
 
       {!isLoading && profile && (
-        <ContainerFooter>
-          <CardFooter>
-            <ConatainerLang>
-              <Typography sx={{ fontSize: '0.75rem', fontWeight: 500 }}>{t('language')}</Typography>
-              {lang === 'fr' ? (
-                <FlagIcon>
-                  <FR />
-                </FlagIcon>
-              ) : (
-                <FlagIcon>
-                  <US />
-                </FlagIcon>
-              )}
-            </ConatainerLang>
-          </CardFooter>
-        </ContainerFooter>
+        <CardFooter className="flex items-center gap-4 p-0">
+          <div className="flex items-center gap-2">
+            <span className="text-xs font-medium">{t('language')}</span>
+            <div>{lang}</div>
+          </div>
+        </CardFooter>
       )}
     </Card>
   );
-};
-
-const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-  gap: 0.2rem;
-`;
-
-const CardFooter = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const ContainerFooter = styled.div`
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-`;
-
-const ConatainerLang = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-`;
-
-const FlagIcon = styled.div`
-  width: 20px;
-  height: 20px;
-
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-`;
+}
 
 export default RandomCard;
